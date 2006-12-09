cdc749ca8b4cbe978c1d320a0b7775b7b421bf62
curiousdannii <curiousdannii@de53ad7e-2520-0410-9dc1-5529d7a2af9a>
2006-12-09 05:12:32 +0000

    + Base: Generate the pretty URLs of the forum's boards. (install.php)  + Base: Included an example .htaccess file (example.htaccess)

---
+++ trunk/Base/install.php
@@ -0,0 +1,43 @@
+<?php
+
+/*******************************************************************************	ATTENTION: If you are trying to install this manually, you should try
+	the package manager. If that does not work, you can run this file by
+	accessing it by url. Please ensure it is in the same location as your
+	forum's SSI.php file.
+*******************************************************************************/
+
+//	Pretty URLs mod v0.1
+
+//	If SSI.php is in the same place as this file, and SMF isn't defined, this is being run standalone.
+if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
+	require_once(dirname(__FILE__) . '/SSI.php');
+//	Hmm... no SSI.php and no SMF?
+elseif (!defined('SMF'))
+	die('<b>Error:</b> Cannot install - please verify you put this in the same place as SMF\'s SSI.php.');
+
+require_once($sourcedir . '/Subs-PrettyUrls.php');
+
+//	Get the current pretty board urls, or make a new array if there are none
+$pretty_board_urls = isset($modSettings['pretty_board_urls']) ? unserialize($modSettings['pretty_board_urls']) : array();
+
+//	Get the board names
+$query = db_query("
+	SELECT ID_BOARD, name
+	FROM {$db_prefix}boards", __FILE__, __LINE__);
+
+while ($row = mysql_fetch_assoc($query))
+{
+//	Don't replace the board urls if they already exist
+	if (!isset($pretty_board_urls[$row['ID_BOARD']]) || $pretty_board_urls[$row['ID_BOARD']] == '')
+	{
+		$pretty_text = generatePrettyUrl($row['name']);
+//		Can't be empty, can't be a number and can't be the same as another
+		if ($pretty_text != '' && !is_numeric($pretty_text) && !array_search($pretty_text, $pretty_board_urls))
+			$pretty_board_urls[$row['ID_BOARD']] = $pretty_text;
+	}
+}
+
+//	Update the settings table
+updateSettings(array('pretty_board_urls' => serialize($pretty_board_urls)));
+
+?>
