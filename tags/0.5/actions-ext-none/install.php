dcdca838f0665fd96d193d660779cd16cd149e05
curiousdannii <curiousdannii@de53ad7e-2520-0410-9dc1-5529d7a2af9a>
2007-04-08 04:46:24 +0000

    Tagging 0.5.

---
+++ tags/0.5/actions-ext-none/install.php
@@ -0,0 +1,30 @@
+<?php
+
+/*******************************************************************************	ATTENTION: If you are trying to install this manually, you should try
+	the package manager. If that does not work, you can run this file by
+	accessing it by url. Please ensure it is in the same location as your
+	forum's SSI.php file.
+*******************************************************************************/
+
+//	Pretty URLs - actions-ext-none v0.5
+
+//	If SSI.php is in the same place as this file, and SMF isn't defined, this is being run standalone.
+if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
+	require_once(dirname(__FILE__) . '/SSI.php');
+//	Hmm... no SSI.php and no SMF?
+elseif (!defined('SMF'))
+	die('<b>Error:</b> Cannot install - please verify you put this in the same place as SMF\'s SSI.php.');
+
+//	Add the filter callback
+$filter_callbacks = unserialize($modSettings['pretty_filter_callbacks']);
+$filter_callbacks[50] = array('PrettyUrls-Filters.php', 'pretty_urls_actions_filter');
+ksort($filter_callbacks);
+
+//	Update the settings table
+updateSettings(array('pretty_filter_callbacks' => serialize($filter_callbacks)));
+
+//	Clear the URLs cache
+db_query("
+	TRUNCATE TABLE {$db_prefix}pretty_urls_cache", __FILE__, __LINE__);
+
+?>
