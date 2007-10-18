b9cebe5c5d7fc1e552de96f953037b80153ef8fe
naocyna@gmail.com <naocyna@gmail.com@de53ad7e-2520-0410-9dc1-5529d7a2af9a>
2007-10-18 08:34:17 +0000

    This is my first attempt to work with Subversion so please bear with me if I'm doing it wrong ;) smf-2.0 is the SMF 2.0 Beta 1.1 version of the Pretty URLs mod. It is also used to develop new features for the 1.1.x version.

---
+++ trunk/smf-2.0/uninstall.php
@@ -0,0 +1,21 @@
+<?php
+
+/*******************************************************************************	ATTENTION: If you are trying to install this manually, you should try
+	the package manager. If that does not work, you can run this file by
+	accessing it by url. Please ensure it is in the same location as your
+	forum's SSI.php file.
+*******************************************************************************/
+
+//	Pretty URLs - Base v0.8
+
+//	If SSI.php is in the same place as this file, and SMF isn't defined, this is being run standalone.
+if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
+	require_once(dirname(__FILE__) . '/SSI.php');
+//	Hmm... no SSI.php and no SMF?
+elseif (!defined('SMF'))
+	die('<b>Error:</b> Cannot uninstall - please verify you put this in the same place as SMF\'s SSI.php.');
+
+//	Don't rewrite anything for this page
+$modSettings['pretty_enable_filters'] = false;
+
+?>
