5c4f4a4cdca2629df3df61c0e0ae1589d5030d8f
curiousdannii <curiousdannii@de53ad7e-2520-0410-9dc1-5529d7a2af9a>
2008-03-21 15:29:20 +0000

    Tagging 0.8.1

---
+++ tags/0.8.1/Base/SMF1-1/PrettyUrls.php
@@ -0,0 +1,192 @@
+<?php
+//	Version: 0.9; PrettyUrls
+
+if (!defined('SMF'))
+	die('Hacking attempt...');
+
+//	Shell for all the Pretty URL interfaces
+function PrettyInterface()
+{
+	global $context, $scripturl, $settings, $txt;
+
+	//	Keep the critters out
+	isAllowedTo('admin_forum');
+
+	//	Default templating stuff
+	loadTemplate('PrettyUrls');
+	if (loadLanguage('PrettyUrls') == false)
+		loadLanguage('PrettyUrls', 'english');
+
+	//	Shiny chrome interface
+	adminIndex('config');
+	$context['template_layers']['pretty_chrome'] = 'pretty_chrome';
+	$context['html_headers'] .= '
+	<link rel="stylesheet" type="text/css" href="' . $settings['default_theme_url'] . '/pretty/chrome.css" media="screen,projection" />';
+	$context['pretty']['chrome'] = array(
+		'menu' => array(
+			'settings' => array(
+				'href' => $scripturl . '?action=admin;area=pretty',
+				'title' => $txt['pretty_chrome_menu_settings'],
+			),
+			'maintenance' => array(
+				'href' => $scripturl . '?action=admin;area=pretty;sa=maintenance',
+				'title' => $txt['pretty_chrome_menu_maintenance'],
+			),
+		),
+	);
+
+	//	What can we do today?
+	$subActions = array(
+		'filters' => 'pretty_edit_filters',
+		'maintenance' => 'pretty_maintenance',
+		'settings' => 'pretty_manage_settings',
+	);
+	if (isset($_REQUEST['sa']) && isset($subActions[$_REQUEST['sa']]))
+		call_user_func($subActions[$_REQUEST['sa']]);
+	else
+		pretty_manage_settings();
+}
+
+//	An interface to manage the settings and filters
+function pretty_manage_settings()
+{
+	global $context, $modSettings, $settings, $sourcedir, $txt;
+
+	//	Core settings
+	$context['pretty']['settings']['core'] = array(
+		array(
+			'id' => 'pretty_enable_filters',
+			'label' => $txt['pretty_enable'],
+			'type' => 'text',
+			'value' => $modSettings['pretty_enable_filters'],
+		),
+	);
+
+	//	Load the filters data
+	$context['pretty']['filters'] = unserialize($modSettings['pretty_filters']);
+
+	//	Are we saving settings now?
+	if (isset($_REQUEST['save']))
+	{
+		//	Get each filter from the form and save them
+		foreach ($context['pretty']['filters'] as $id => $filter)
+			$context['pretty']['filters'][$id]['enabled'] = isset($_POST['pretty_filter_' . $id]) ? 1 : 0;
+
+		$pretty_settings = array(
+			'pretty_enable_filters' => $_POST['pretty_enable'],
+			'pretty_filters' => addslashes(serialize($context['pretty']['filters'])),
+		);
+		updateSettings($pretty_settings);
+
+		//	Update the filters too
+		require_once($sourcedir . '/Subs-PrettyUrls.php');
+		pretty_update_filters();
+
+		//	All finished now!
+		$_SESSION['pretty']['notice'] = 'Settings saved';
+		redirectexit('action=admin;area=pretty');
+	}
+
+	//	Action-specific chrome
+	$context['page_title'] = $txt['pretty_chrome_page_title_settings'];
+	$context['sub_template'] = 'pretty_settings';
+	$context['pretty']['chrome']['title'] = $txt['pretty_chrome_menu_settings'];
+	$context['pretty']['chrome']['caption'] = $txt['pretty_chrome_caption_settings'];
+
+	//	Load the settings up
+	$context['pretty']['settings']['enable'] = $modSettings['pretty_enable_filters'];
+
+	//	Any notices?
+	if (isset($_SESSION['pretty']['notice']))
+	{
+		$context['pretty']['chrome']['notice'] = $_SESSION['pretty']['notice'];
+		unset($_SESSION['pretty']['notice']);
+	}
+}
+
+//	Interface for URL maintenance
+function pretty_maintenance()
+{
+	global $context, $sourcedir, $txt;
+
+	//	Run the maintenance tasks
+	if (isset($_REQUEST['run']))
+	{
+		require_once($sourcedir . '/Subs-PrettyUrls.php');
+		pretty_run_maintenance();
+	}
+
+	//	Action-specific chrome
+	$context['page_title'] = $txt['pretty_chrome_page_title_maintenance'];
+	$context['sub_template'] = 'pretty_maintenance';
+	$context['pretty']['chrome']['title'] = $txt['pretty_chrome_menu_maintenance'];
+	$context['pretty']['chrome']['caption'] = $txt['pretty_chrome_caption_maintenance'];
+}
+
+//	Interface to edit the filters array
+function pretty_edit_filters()
+{
+	global $context, $modSettings, $sourcedir, $txt;
+
+	//	Check the JSON extension is installed
+	if (!function_exists('json_encode'))
+	{
+		unset($context['template_layers']['pretty_chrome']);
+		fatal_lang_error('pretty_no_json', false);
+	}
+
+	//	Save the filters array
+	if (isset($_REQUEST['save']))
+	{
+		//	Try to process the edited JSON array
+		$json_filters = (isset($_POST['pretty_json_filters'])) ? $_POST['pretty_json_filters'] : '';
+		$json_filters = stripslashes($json_filters);
+		$filters_array = json_decode($json_filters, true);
+
+		//	Was that successful or not?
+		if ($filters_array == NULL)
+		{
+			$_SESSION['pretty']['notice'] = 'There was an error with the JSON array you submitted';
+			$_SESSION['pretty']['json_filters'] = $json_filters;
+		}
+		else
+		{
+			require_once($sourcedir . '/Subs-PrettyUrls.php');
+			updateSettings(array('pretty_filters' => addslashes(serialize($filters_array))));
+			pretty_update_filters();
+			$_SESSION['pretty']['notice'] = 'Filters saved and updated';
+		}
+
+		redirectexit('action=admin;area=pretty;sa=filters');
+	}
+
+	//	Action-specific chrome
+	$context['page_title'] = $txt['pretty_chrome_page_title_filters'];
+	$context['sub_template'] = 'pretty_filters';
+	$context['pretty']['chrome']['title'] = $txt['pretty_chrome_title_filters'];
+	$context['pretty']['chrome']['caption'] = $txt['pretty_chrome_caption_filters'];
+
+	if (isset($_SESSION['pretty']['json_filters']))
+	{
+		//	We're working on something already
+		$context['pretty']['json_filters'] = $_SESSION['pretty']['json_filters'];
+		unset($_SESSION['pretty']['json_filters']);
+	}
+	else
+	{
+		//	Convert the filters array to JSON and format it nicely
+		require_once($sourcedir . '/Subs-PrettyUrls.php');
+		$context['pretty']['json_filters'] = json_encode(unserialize($modSettings['pretty_filters']));
+		$context['pretty']['json_filters'] = pretty_json($context['pretty']['json_filters']);
+		$context['pretty']['json_filters'] = str_replace('\/', '/', $context['pretty']['json_filters']);
+	}
+
+	//	Any new notices?
+	if (isset($_SESSION['pretty']['notice']))
+	{
+		$context['pretty']['chrome']['notice'] = $_SESSION['pretty']['notice'];
+		unset($_SESSION['pretty']['notice']);
+	}
+}
+
+?>
---
+++ tags/0.8.1/Base/SMF2-0/install.php
@@ -0,0 +1,169 @@
+<?php
+
+/*******************************************************************************
+	ATTENTION: If you are trying to install this manually, you should try
+	the package manager. If that does not work, you can run this file by
+	accessing it by url. Please ensure it is in the same location as your
+	forum's SSI.php file.
+*******************************************************************************/
+
+//	Pretty URLs - Base v0.8.1
+
+//	If SSI.php is in the same place as this file, and SMF isn't defined, this is being run standalone.
+if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
+{
+	require_once(dirname(__FILE__) . '/SSI.php');
+	$standalone = true;
+}
+//	Hmm... no SSI.php and no SMF?
+elseif (!defined('SMF'))
+	die('<b>Error:</b> Cannot install - please verify you put this in the same place as SMF\'s SSI.php.');
+
+//	Get the list of tasks ready
+$tasks = array(
+	'db' => 'Database modifications',
+	'dbchanges' => array(),
+);
+
+//	Create the pretty_topic_urls table
+$smcFunc['db_create_table']('pretty_topic_urls', array(
+	array('name' => 'id_topic', 'type' => 'mediumint'),
+	array('name' => 'pretty_url', 'type' => 'varchar', 'size' => 80),
+), array(
+	array('type' => 'primary', 'columns' => array('id_topic')),
+	array('type' => 'unique', 'columns' => array('pretty_url')),
+));
+$tasks['dbchanges'][] .= 'Creating the pretty_topic_urls table';
+
+/*	Doesn't work in SQLite yet
+//	Fix old topics by replacing ' with chr(18)
+$smcFunc['db_query']('', "
+	UPDATE {db_prefix}pretty_topic_urls
+	SET pretty_url = REPLACE(pretty_url, {string:old_quote}, {string:new_quote})",
+array(
+	'old_quote' => "'",
+	'new_quote' => chr(18),
+));
+$tasks['dbchanges'][] = 'Fixing any old topics with broken quotes';
+*/
+
+//	Create the pretty_urls_cache table
+$smcFunc['db_create_table']('pretty_urls_cache', array(
+	array('name' => 'url_id', 'type' => 'varchar', 'size' => 255),
+	array('name' => 'replacement', 'type' => 'varchar', 'size' => 255),
+	array('name' => 'log_time', 'type' => 'int', 'size' => 10),
+), array(
+	array('type' => 'primary', 'columns' => array('url_id')),
+), NULL, 'overwrite');
+$tasks['dbchanges'][] .= 'Creating the pretty_urls_cache table';
+
+//	Default filter settings
+$prettyFilters = array(
+	'boards' => array(
+		'description' => 'Rewrite Board URLs',
+		'enabled' => 1,
+		'filter' => array(
+			'priority' => 45,
+			'callback' => 'pretty_urls_board_filter',
+		),
+		'rewrite' => array(
+			'priority' => 40,
+			'rule' => 'RewriteRule ^ROOTURL([-_!~*\'()$a-zA-Z0-9]+)/?$ ./index.php?pretty;board=$1.0 [L,QSA]
+RewriteRule ^ROOTURL([-_!~*\'()$a-zA-Z0-9]+)/([0-9]*)/?$ ./index.php?pretty;board=$1.$2 [L,QSA]',
+		),
+		'title' => 'Boards',
+	),
+	'topics' => array(
+		'description' => 'Rewrite Topic URLs',
+		'enabled' => 1,
+		'filter' => array(
+			'priority' => 40,
+			'callback' => 'pretty_urls_topic_filter',
+		),
+		'rewrite' => array(
+			'priority' => 45,
+			'rule' => 'RewriteRule ^ROOTURL([-_!~*\'()$a-zA-Z0-9]+)/([-_!~*\'()$a-zA-Z0-9]+)/?$ ./index.php?pretty;board=$1;topic=$2.0 [L,QSA]
+RewriteRule ^ROOTURL([-_!~*\'()$a-zA-Z0-9]+)/([-_!~*\'()$a-zA-Z0-9]+)/([0-9]*|msg[0-9]*|new)/?$ ./index.php?pretty;board=$1;topic=$2.$3 [L,QSA]',
+		),
+		'title' => 'Topics',
+	),
+	'actions' => array(
+		'description' => 'Rewrite Action URLs (ie, index.php?action=something)',
+		'enabled' => 1,
+		'filter' => array(
+			'priority' => 90,
+			'callback' => 'pretty_urls_actions_filter',
+		),
+		'rewrite' => array(
+			'priority' => 20,
+			'rule' => '#ACTIONS',	//	To be replaced in pretty_update_filters()
+		),
+		'title' => 'Actions',
+	),
+	'profiles' => array(
+		'description' => 'Rewrite Profile URLs. As this uses the Username of an account rather than it\'s Display Name, it may not be desirable to your users.',
+		'enabled' => 0,
+		'filter' => array(
+			'priority' => 80,
+			'callback' => 'pretty_profiles_filter',
+		),
+		'rewrite' => array(
+			'priority' => 15,
+			'rule' => 'RewriteRule ^profile/([^/]+)/?$ ./index.php?pretty;action=profile;user=$1 [L,QSA]',
+		),
+		'title' => 'Profiles',
+	),
+);
+$tasks[] = 'Adding the default filters';
+
+//	Add the pretty_root_url and pretty_enable_filters settings:
+$pretty_root_url = isset($modSettings['pretty_root_url']) ? $modSettings['pretty_root_url'] : $boardurl;
+$pretty_enable_filters = isset($modSettings['pretty_enable_filters']) ? $modSettings['pretty_enable_filters'] : 0;
+
+//	Update the settings table
+updateSettings(array(
+	'pretty_enable_filters' => $pretty_enable_filters,
+	'pretty_filters' => serialize($prettyFilters),
+	'pretty_root_url' => $pretty_root_url,
+));
+$tasks[] = 'Adding some settings';
+
+//	Run maintenance
+require_once($sourcedir . '/Subs-PrettyUrls.php');
+pretty_run_maintenance();
+$tasks[] = 'Running maintenance tasks';
+$tasks[] = $context['pretty']['maintenance_tasks'];
+
+//	Format the tasks list
+$first = true;
+$task_list = '<ul>';
+foreach ($tasks as $task)
+	if (is_array($task))
+	{
+		$task_list .= '<ul>';
+		foreach ($task as $subtask)
+			$task_list .= '<li>' . $subtask . '</li>';
+		$task_list .= '</ul>';
+	}
+	else
+	{
+		if ($first = true)
+			$first = false;
+		else
+			$task_list .= '</li>';
+		$task_list .= '<li>' . $task;
+	}
+$task_list .= '</li></ul>';
+
+//	Output the list of database changes
+if (isset($standalone))
+{
+	echo '<title>Installing Pretty URLs - Base 0.8.1</title>
+<h1>Installing Pretty URLs - Base 0.8.1</h1>
+<h2>Database changes</h2>
+', $task_list ;
+}
+else
+	$txt['package_installed_done'] = $task_list . $txt['package_installed_done'];
+
+?>
