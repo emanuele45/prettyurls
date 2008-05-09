dfbe5ced1b24e101e18b925119bb5decab7941a2
curiousdannii <curiousdannii@de53ad7e-2520-0410-9dc1-5529d7a2af9a>
2008-05-09 03:18:21 +0000

    % Base: Updated for SMF 2.0 Beta 3.1 Public.

--- branches/0.8/Base/SMF2-0/install.php
+++ branches/0.8/Base/SMF2-0/install.php
@@ -7,12 +7,13 @@
 	forum's SSI.php file.
 *******************************************************************************/
 
-//	Pretty URLs - Base v0.8.2
+//	Pretty URLs - Base v0.8.3
 
 //	If SSI.php is in the same place as this file, and SMF isn't defined, this is being run standalone.
 if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
 {
 	require_once(dirname(__FILE__) . '/SSI.php');
+	db_extend('packages');
 	$standalone = true;
 }
 //	Hmm... no SSI.php and no SMF?
@@ -157,8 +158,8 @@ $task_list .= '</li></ul>';
 //	Output the list of database changes
 if (isset($standalone))
 {
-	echo '<title>Installing Pretty URLs - Base 0.8.2</title>
-<h1>Installing Pretty URLs - Base 0.8.2</h1>
+	echo '<title>Installing Pretty URLs - Base 0.8.3</title>
+<h1>Installing Pretty URLs - Base 0.8.3</h1>
 <h2>Database changes</h2>
 ', $task_list ;
 }
