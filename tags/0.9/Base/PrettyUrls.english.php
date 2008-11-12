f1cf5df900c92e9aa6f8ccae80f30c5963907ad6
curiousdannii <curiousdannii@de53ad7e-2520-0410-9dc1-5529d7a2af9a>
2008-11-12 07:52:40 +0000

    Branching and tagging 0.9

---
+++ tags/0.9/Base/PrettyUrls.english.php
@@ -0,0 +1,39 @@
+<?php
+//	Version: 0.9; PrettyUrls
+
+//	Admin chrome
+$txt['pretty_chrome_title'] = 'Pretty URLs Administration';
+$txt['pretty_chrome_menu_news'] = 'News';
+$txt['pretty_chrome_menu_settings'] = 'Settings';
+$txt['pretty_chrome_menu_maintenance'] = 'Maintenance';
+
+//	News page
+$txt['pretty_chrome_caption_news'] = 'The latest news and information live from the project website';
+$txt['pretty_chrome_page_title_news'] = 'Pretty URLs News &amp; Info';
+$txt['pretty_chrome_title_news'] = 'News &amp; Info';
+$txt['pretty_current_version'] = 'Current version';
+$txt['pretty_download'] = 'Download';
+$txt['pretty_latest_version'] = 'Latest version';
+$txt['pretty_upgrade'] = 'Upgrade now!';
+$txt['pretty_version'] = 'Version info';
+
+//	Settings page
+$txt['pretty_chrome_caption_settings'] = 'Settings caption';
+$txt['pretty_chrome_page_title_settings'] = 'Pretty URLs Settings';
+$txt['pretty_core_settings'] = 'Core settings';
+$txt['pretty_enable'] = 'Enable URL rewriting';
+$txt['pretty_filters'] = 'URL rewriting filters';
+$txt['pretty_save'] = 'Save changes';
+
+//	Maintenance page
+$txt['pretty_chrome_caption_maintenance'] = 'Run some maintenance tasks';
+$txt['pretty_chrome_page_title_maintenance'] = 'Pretty URLs Maintenance';
+$txt['pretty_run_maintenance'] = 'Run maintenance tasks';
+
+//	Edit filters page
+$txt['pretty_chrome_caption_filters'] = 'A tool to edit the filters array. The array is formatted here in the <a href="http://www.json.org/">JSON</a> format. Be careful, the JSON parser is rather strict.';
+$txt['pretty_chrome_page_title_filters'] = 'Pretty URLs Filters Tool';
+$txt['pretty_chrome_title_filters'] = 'Filters Tool';
+$txt['pretty_no_json'] = 'The PHP JSON extension is required for this tool. Consider upgrading to PHP 5.2.';
+
+?>
