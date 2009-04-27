a37435a9d103c7cf9770a738e90cfcf37a3596cb
curiousdannii <curiousdannii@de53ad7e-2520-0410-9dc1-5529d7a2af9a>
2009-04-27 14:42:17 +0000

    News: notify people about the current bugs

--- trunk/news.js
+++ trunk/news.js
@@ -3,6 +3,7 @@
 var chrome = chrome || {};
 
 chrome.news = [
+	{date: '28 April 2009', text: 'Pretty URLs 0.9.2 has several significant bugs, and it is recommended to go back to 0.9, which you can get at the downloads page. Sorry if you have experienced these bugs, I hope to fix them soon. Also, all versions of Pretty URLs break the noindex check, meaning all topics and boards will not be indexed. If this matters to you, please remove the meta tag from your index template, or disable this mod!'},
 	{date: '25 March 2009', text: 'Pretty URLs 0.9.2 released'},
 	{date: '24 Feb 2009', text: 'Pretty URLs 0.9.1 released'},
 	{date: '12 Nov 2008', text: 'Pretty URLs 0.9 released'}	
