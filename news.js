0653c6f73725efc2ff98aad09ec501e7f47a0f9f
curiousdannii <curiousdannii@de53ad7e-2520-0410-9dc1-5529d7a2af9a>
2009-04-28 14:47:08 +0000

    News: remind people about the reverter package

--- news.js
+++ news.js
@@ -3,7 +3,7 @@
 var chrome = chrome || {};
 
 chrome.news = [
-	{date: '28 April 2009', text: 'Pretty URLs 0.9.2 has several significant bugs, and it is recommended to go back to 0.9, which you can get at the downloads page. Sorry if you have experienced these bugs, I hope to fix them soon. Also, all versions of Pretty URLs break the noindex check, meaning all topics and boards will not be indexed. If this matters to you, please remove the meta tag from your index template, or disable this mod!'},
+	{date: '28 April 2009', text: 'Pretty URLs 0.9.2 has several significant bugs, and it is recommended to go back to 0.9, which you can get at the downloads page. Sorry if you have experienced these bugs, I hope to fix them soon.<br/>Also, all versions of Pretty URLs break the noindex check, meaning all topics and boards will not be indexed. If this matters to you, please remove the meta tag from your index template, or disable this mod. If you decide to uninstall this mod, the Reverter package is essential! Get it at the <a href="http://code.google.com/p/prettyurls/downloads/list">downloads page</a>.'},
 	{date: '25 March 2009', text: 'Pretty URLs 0.9.2 released'},
 	{date: '24 Feb 2009', text: 'Pretty URLs 0.9.1 released'},
 	{date: '12 Nov 2008', text: 'Pretty URLs 0.9 released'}	
