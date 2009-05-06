290bf4f620e571ba3b8098ff13bd70b626312b27
curiousdannii <curiousdannii@de53ad7e-2520-0410-9dc1-5529d7a2af9a>
2009-05-06 11:38:48 +0000

    Tagging 0.9.3

---
+++ tags/0.9.3/news.js
@@ -0,0 +1,11 @@
+//	Pretty URLs live news
+
+var chrome = chrome || {};
+
+chrome.news = [
+	{date: '12 Nov 2008', text: 'Pretty URLs 0.9 released'}	
+];
+
+chrome.latestVersion = '0.9';
+chrome.upgradeFrom = '';
+chrome.upgradeUrl = '';
