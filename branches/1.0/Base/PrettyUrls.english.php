ef1bd53e41f3dfbd417cc2ff4b90a618d86c77fb
curiousdannii <curiousdannii@de53ad7e-2520-0410-9dc1-5529d7a2af9a>
2009-06-19 06:19:41 +0000

    Branching and tagging 1.0RC

---
+++ branches/1.0/Base/PrettyUrls.english.php
@@ -0,0 +1,48 @@
+<?php
+//	Version: 1.0RC; PrettyUrls
+
+// Install chrome
+$txt['pretty_chrome_install_title'] = 'Installing Pretty URLs';
+$txt['pretty_install_success'] = 'Thank you for using <b>Pretty URLs</b>! The installation was a success!';
+$txt['pretty_install_continue'] = 'Please continue to the admin page, where you can enable URL rewriting.';
+
+//	Admin chrome
+$txt['pretty_chrome_title'] = 'Pretty URLs Administration';
+$txt['pretty_chrome_menu_news'] = 'News';
+$txt['pretty_chrome_menu_settings'] = 'Settings';
+$txt['pretty_chrome_menu_maintenance'] = 'Maintenance';
+
+//	News page
+$txt['pretty_chrome_caption_news'] = 'The latest news and information live from the project website';
+$txt['pretty_chrome_page_title_news'] = 'Pretty URLs News &amp; Info';
+$txt['pretty_chrome_title_news'] = 'News &amp; Info';
+$txt['pretty_current_version'] = 'Current version';
+$txt['pretty_download'] = 'Download';
+$txt['pretty_latest_version'] = 'Latest version';
+$txt['pretty_upgrade'] = 'Upgrade now!';
+$txt['pretty_version'] = 'Version info';
+
+//	Settings page
+$txt['pretty_cant_write_htaccess'] = 'Cannot overwrite existing .htaccess file. Please change its write permissions.';
+$txt['pretty_chrome_caption_settings'] = 'Various settings';
+$txt['pretty_chrome_page_title_settings'] = 'Pretty URLs Settings';
+$txt['pretty_core_settings'] = 'Core settings';
+$txt['pretty_enable'] = 'Enable URL rewriting';
+$txt['pretty_filters'] = 'URL rewriting filters';
+$txt['pretty_save'] = 'Save changes';
+
+// Tests page
+$txt['pretty_chrome_caption_tests'] = 'Before enabling URL rewriting, please check that these rewritten links work.';
+
+//	Maintenance page
+$txt['pretty_chrome_caption_maintenance'] = 'Run some maintenance tasks';
+$txt['pretty_chrome_page_title_maintenance'] = 'Pretty URLs Maintenance';
+$txt['pretty_run_maintenance'] = 'Run maintenance tasks';
+
+//	Edit filters page
+$txt['pretty_chrome_caption_filters'] = 'A tool to edit the filters array. The array is formatted here in the <a href="http://www.json.org/">JSON</a> format. Be careful, the JSON parser is rather strict.';
+$txt['pretty_chrome_page_title_filters'] = 'Pretty URLs Filters Tool';
+$txt['pretty_chrome_title_filters'] = 'Filters Tool';
+$txt['pretty_no_json'] = 'The PHP JSON extension is required for this tool. Consider upgrading to PHP 5.2.';
+
+?>
---
+++ tags/1.0RC/Base/uninstall.php
@@ -0,0 +1,9 @@
+<?php
+
+//	Pretty URLs - Base v0.8.1
+
+//	All I do is disable URL rewriting. If you're uninstalling the mod manually, please just ignore me.
+
+$modSettings['pretty_enable_filters'] = false;
+
+?>
